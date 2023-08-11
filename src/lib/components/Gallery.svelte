d2b75c6aa14a3dc138d96975834559ac0a3748e1
Amanda Viray <amandaviray@yahoo.com>
2023-08-11 12:56:00 +1000

    Resolved Gallery Component

--- src/lib/components/Gallery.svelte
+++ src/lib/components/Gallery.svelte
@@ -1,24 +1,23 @@
- 
-<script>
+ <script>
     import { storyblokEditable } from '@storyblok/svelte';
     export let blok;
     let images = [];
     blok.images.map((img) =>
     images.push( 
         {
-        original: img.filename,
-        thumbnail:img.filename,
-        originalAlt: img.alt,
+        url: img.filename,
+        alt: img.alt,
         }
-  ))
+    ))	
 </script>
+
 <div use:storyblokEditable={blok}>
     <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
         <div class="-m-1 flex flex-wrap md:-m-2">
             {#each images as img}
             <div class="flex w-1/3 flex-wrap">
             <img
-            src="{img.filename}/m/1600x0"
+            src={img.url}
             alt={img.alt}
             />
             </div>
--- src/lib/sblib.js
+++ src/lib/sblib.js
@@ -1,7 +1,12 @@
-import { apiPlugin, storyblokInit } from '@storyblok/svelte';
+import { apiPlugin, storyblokInit, RichTextSchema } from '@storyblok/svelte';
 // 001 Import the environment variables
 import { PUBLIC_ACCESS_TOKEN } from '$env/static/public';
 import { PUBLIC_REGION } from '$env/static/public';
+
+import cloneDeep from "clone-deep";
+const mySchema = cloneDeep(RichTextSchema); // you can make a copy of the default RichTextSchema
+// ... and edit the nodes and marks, or add your own.
+// Check the base RichTextSchema source here https://github.com/storyblok/storyblok-js-client/blob/master/source/schema.js
 
 export async function useStoryblok(accessToken = '') {
 	// 002 setting the access token (from environment variable)
@@ -23,8 +28,8 @@ export async function useStoryblok(accessToken = '') {
             'all-projects': (await import('$lib/components/AllProjects.svelte')).default,
             'project-highlights': (await import('$lib/components/ProjectHighlights.svelte')).default,
             'rich-text': (await import('$lib/components/Rich-Text.svelte')).default,
-            'grid_item_report': (await import('$lib/components/GridReportItem.svelte')).default,
-            'gallery': (await import('$lib/components/Gallery.svelte')).default,
+            grid_item_report: (await import('$lib/components/GridReportItem.svelte')).default,
+            gallery: (await import('$lib/components/Gallery.svelte')).default,
 		},
 		// 007 setting some api options like https, cache and region
 		apiOptions: {
@@ -33,6 +38,61 @@ export async function useStoryblok(accessToken = '') {
 				type: 'memory'
 			},
 			region: PUBLIC_REGION // "us" if your space is in US region
-		}
+		},
+		richText: {
+			schema: mySchema,
+			resolver: (component, blok) => {
+			  switch (component) {
+				case "gallery":
+					if (blok.type == "carousel" || blok.type == undefined) {
+						const images = blok.images.map((item, i, img) => {
+							const previousItem = i > 0 ? img[i - 1] : null;
+							const nextItem = i < img.length - 1 ? img[i + 1] : null;
+						
+							const prev = previousItem
+								? `<a href="#${previousItem.id}" class="btn btn-circle">❮</a>`
+								: `<a href="#" class="btn btn-circle bg-[#94a3b8]">❮</a>`;
+							
+							const next = nextItem
+								? `<a href="#${nextItem.id}" class="btn btn-circle">❯</a>`
+								: `<a class="btn btn-circle hover:bg-sky-700 bg-[#94a3b8]">❯</a>`;
+						
+							return `<div id="${item.id}" class="carousel-item relative w-full h-full">
+								<img src="${item.filename}" alt="${item.alt}" class="w-full h-auto object-cover"/>
+								<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+									${prev}
+									${next}
+								</div>
+							</div>`;
+						});
+						const galleries = images.join('');
+	
+						return `
+						<div class="carousel w-full">
+							${galleries}
+						</div>`
+					};
+
+					if (blok.type = "sponsors") {
+							const images = blok.images.map((item) => {
+								return ` 
+								<div id="${item.id}" class="w-full p-2">
+								<img src="${item.filename}" alt="${item.alt}" class="w-full h-auto object-cover"/>
+								</div>`;
+							});
+							const galleries = images.join('');
+		
+							return `
+							<div class="carousel w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    						${galleries}
+							</div>`
+					};
+				case "video":
+					return null
+				default:
+				  return "Resolver not defined";
+			  }
+			},
+		},
 	});
 }
