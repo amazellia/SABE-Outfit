b8fadb85afc6f06741206c295e5a531b45196a20
Amanda Viray <amandaviray@yahoo.com>
2023-08-15 10:30:08 +1000

    Moved nav buttons to the right side

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -20,14 +20,6 @@
             />
         </a> 
         {/if}
-        <button class="lg:hidden" on:click={toggleNav}> <!-- This button will show on mobile only -->
-            <svg class="w-6 h-6" class:close={navIsOpen} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
-            </svg>
-            <svg class="w-6 h-6 hidden" class:open={navIsOpen} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-            </svg>
-        </button>
         {#if header}
         <nav class="hidden lg:block" class:open={navIsOpen}>
             <ul class="flex space-x-4 lg:space-x-8 text-lg font-bold">
@@ -39,6 +31,14 @@
             </ul>
         </nav>
         {/if}
+        <button class="lg:hidden" on:click={toggleNav}> <!-- This button will show on mobile only -->
+            <svg class="w-6 h-6" class:close={navIsOpen} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
+            <svg class="w-6 h-6 hidden" class:open={navIsOpen} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+            </svg>
+        </button>
     </div>
 </header>
 
